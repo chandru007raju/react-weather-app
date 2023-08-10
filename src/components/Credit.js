87ce078fbab3250292db91ab715892c6b754b4e1
sr.chandrashekar <chandru007.raju@gmail.com>
2023-08-10 06:41:33 +0530

    pushed project to github

---
+++ src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      <a href="https://github.com/chandru007raju"
+        > S R Chandrashekar</a
+      >, <a href="https://github.com/chandru007raju/React-Weather-App">Open sourced on Github.</a> Hosted on Netlify
+    </p>
+  </div>
+  );
+}
